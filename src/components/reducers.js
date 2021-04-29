f22de3388dbff1dfa1dcce5441eeb84e8396aedd
Kamil_IT <kamillo.gronek@gmail.com>
2021-04-29 12:21:18 +0200

    fix style app

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -9,39 +9,33 @@ export const initialState = {
 export function reducer(prevState,action){
    switch(action.type) {
      case 'FETCH_SUCCESS':
-         return{
-        pokemonPack:action.payload,
-        resultsPokemons :[],
-        error: '',
-        totalResults: 0
-       }
+        return{
+          pokemonPack:action.payload,
+          resultsPokemons :[],
+          error: '',
+          totalResults: 0
+         }
       case 'FETCH_ERROR' :
          return{
           pokemonPack:[],
            resultsPokemons :[],
            error: 'Something went wrong'
          }
-      case 'ADD_POKEMON': {
-         const arrayPokemons= {
+      case 'ADD_POKEMON': 
+         return{
            ...prevState,
           pokemonPack: prevState.pokemonPack.filter(pokemonPack => pokemonPack.name !== action.payload[0].name),
           resultsPokemons:[...prevState.resultsPokemons,action.payload[0]],
           error: 'Choose value',
           totalResults: prevState.resultsPokemons.length + 1,
-        }
-        console.log('After ADD_POKEMON: ', arrayPokemons);
-        return arrayPokemons;
-      }
-     case 'DELETE_POKEMON': {
-      const arrayPokemons= {
+          }
+      case 'DELETE_POKEMON': 
+        return {
           ...prevState,
           pokemonPack: [...prevState.pokemonPack, action.payload],
           resultsPokemons: prevState.resultsPokemons.filter(getPokemon => getPokemon.id !== action.payload.id),
           totalResults: prevState.resultsPokemons.length - 1,
-      }
-      console.log('After DELETE_POKEMON: ', arrayPokemons);
-      return arrayPokemons
-     }
+          }
         default:
         return prevState;
      }
